281423a193565ff84a4d8bbb19c8831babea9a15
Crowdbotics <team@crowdbotics.com>
2021-01-14 01:38:15 +0000

    Update models

--- backend/yabisso_23822/wsgi.py
+++ backend/yabisso_23822/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yabisso_23822.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yabisso_23822.settings")
 
 application = get_wsgi_application()
